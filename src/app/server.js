8ae17207f04d79d16dfe62ba648cee0ced37ed05
klenivingeri <libertti@live.com>
2024-05-01 17:46:58 -0300

    fix: vercel

--- src/app/server.js
+++ src/app/server.js
@@ -1,47 +1,50 @@
-"use server"
+"use server";
 import nodemailer from "nodemailer";
-import dotenv  from 'dotenv'
+import dotenv from "dotenv";
 dotenv.config();
 
-export const testEnd = () => ( { user:process.env.USER, pass:process.env.PASS})
-
+export const testEnd = () => ({
+  user: process.env.USER,
+  pass: process.env.PASS,
+});
 
 export const sendEmail = (msgs) => {
-      let htmlChat = ''
-      msgs.map(m => {
-        if(m.msg){
-          if(m.user == 0 ){
-            htmlChat = htmlChat + `<p><b>${m.msg}</b></p>`
-         } else{
-           htmlChat = htmlChat + `<p>${m.msg}</p>`
-         }
-        }
-      } )
+  let htmlChat = "";
+  msgs.map((m) => {
+    if (m.msg) {
+      if (m.user == 0) {
+        htmlChat = htmlChat + `<p><b>${m.msg}</b></p>`;
+      } else {
+        htmlChat = htmlChat + `<p>${m.msg}</p>`;
+      }
+    }
+  });
 
-      var transporter = nodemailer.createTransport({
-        host: "sandbox.smtp.mailtrap.io",
-        port: 2525,
-        auth: {
-          user: process.env.USER,
-          pass: process.env.PASS
-        }
-      });
-      
-      // Definições do e-mail
-      const mailOptions = {
-        from: 'noreplay@celke.com.br',
-        to: '1234@celke.com.br',
-        subject: 'Assunto do e-mail',
-        text: 'Conteúdo do e-mail',
-        html: `${htmlChat}`
-      };
-      
-      // Enviar e-mail
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.error('Erro ao enviar e-mail:', error);
-        } else {
-          console.log('E-mail enviado:', info.response);
-        }
-      });
-}
+  var transporter = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    secure: false,
+    auth: {
+      user: process.env.USER,
+      pass: process.env.PASS,
+    },
+  });
+
+  // Definições do e-mail
+  const mailOptions = {
+    from: "noreplay@celke.com.br",
+    to: "1234@celke.com.br",
+    subject: "Assunto do e-mail",
+    text: "Conteúdo do e-mail",
+    html: `${htmlChat}`,
+  };
+
+  // Enviar e-mail
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.error("Erro ao enviar e-mail:", error);
+    } else {
+      console.log("E-mail enviado:", info.response);
+    }
+  });
+};
